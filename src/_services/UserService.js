adf32a42aa70b12fa0ae928f0ab60b39532d8900
davidpalacin <palacingimenezdavid.fpllefia@gmail.com>
2023-01-15 12:38:44 +0100

    add rent movies feature

--- src/_services/UserService.js
+++ src/_services/UserService.js
@@ -1,10 +1,9 @@
 import axios from "axios";
-import {enviroment} from '../_enviroments/enviroment.js';
+import { enviroment } from "../_enviroments/enviroment.js";
 
 const UserService = {};
 
 UserService.getAllUsers = async (token) => {
-
   const apiUrl = `${enviroment.BASE_URL}/users`;
 
   const config = {
@@ -15,5 +14,17 @@ UserService.getAllUsers = async (token) => {
 
   return res.data;
 };
+
+UserService.rentMovie = async (user, movie) => {
+  try {
+    const apiURL = `${enviroment.BASE_URL}/users/update/${user._id}`;
+    const res = await axios.patch(apiURL, movie);
+  
+    return res.data;
+
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 export default UserService;
--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -1,34 +1,47 @@
 import React from "react";
 import { useState } from "react";
+import { useSelector } from "react-redux";
 import { enviroment } from "../../_enviroments/enviroment";
+import UserService from "../../_services/UserService";
 import "./MovieCard.scss";
 import styled from "styled-components";
 
-export default function MovieCard({ movie }) {
-  const [isModalOpen, setIsModalOpen] = useState(false);
+const ModalContainer = styled.div`
+  top: 50%;
+  left: 50%;
+  right: auto;
+  bottom: auto;
+  margin-right: -50%;
+  transform: translate(-50%, -50%);
+  background-color: white;
+  padding: 20px;
+  border-radius: 10px;
+  position: fixed;
+  z-index: 99999;
+  display: flex;
+  flex-direction: column;
+  gap: 13px;
+  width: 90%;
+  height: fit-content;
+`;
 
-  const ModalContainer = styled.div`
-    top: 50%;
-    left: 50%;
-    right: auto;
-    bottom: auto;
-    margin-right: -50%;
-    transform: translate(-50%, -50%);
-    background-color: white;
-    padding: 20px;
-    border-radius: 10px;
-    position: fixed;
-    z-index: 99999;
-    display: flex;
-    flex-direction: column;
-    gap: 13px;
-    width: 90%;
-    height: fit-content;
-  `;
+export default function MovieCard({ movie }) {
+  const userLogged = useSelector((state) => state.auth.user);
+  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   const handleMovieClick = () => {
     setIsModalOpen(true);
   };
+
+  const handleRentMovie = () => {
+    if(isLoggedIn) {
+      const resultAddMovie = UserService.rentMovie(userLogged, movie);
+      console.log(resultAddMovie);
+    } else {
+      alert("Necesitas iniciar sesión para usar esto.")
+    }
+  }
 
   return (
     <>
@@ -46,6 +59,7 @@ export default function MovieCard({ movie }) {
           <strong>Do you want to rent {movie.title}?</strong>
           <div className="movie-info">
             <img
+             className="modal-image"
               src={`${enviroment.IMAGE_URL}${movie.poster_path}`}
               alt="poster path"
               width={200}
@@ -53,7 +67,7 @@ export default function MovieCard({ movie }) {
             <div className="movie-info-details">
               <span className="movie-info-description">{movie.overview}</span>
               <div className="movie-info-description-buttons">
-                <button onClick={() => setIsModalOpen(false)}>Yes</button>
+                <button onClick={handleRentMovie}>Yes</button>
                 <button onClick={() => setIsModalOpen(false)}>No</button>
               </div>
             </div>
